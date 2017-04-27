bfbb6011bac9ddf02b190e0e2c679e14809b56d8
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-04-27 16:46:21 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # wol-e
-A wake-on-lan manager in vuejs (and electron)
+
+> A wake-on-lan manager in vuejs
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * @param  {string}   name     the filename (basename) of the view to load.
+ */
+function view (name) {
+  return function (resolve) {
+    require(['../views/' + name + '.vue'], resolve)
+  }
+};
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: view('Home')
+    }
+  ]
+})
