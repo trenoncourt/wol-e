5b74cf1696d2bceb0d302dada6cd366231b1b6ce
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-07 18:20:04 +0200

    Add loader.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="row top-lg top-xs">
+    <loader :visible="loading"></loader>
+
     <div v-for="computer in computers" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
       <card class="top"></card>
     </div>
@@ -10,18 +12,21 @@
 <script>
 import WolBtn from '../components/Button.vue'
 import Card from '../components/Card.vue'
+import Loader from '../components/Loader.vue'
 
 export default {
-  components: { WolBtn, Card },
+  components: { WolBtn, Card, Loader },
   data () {
     return {
-      computers: []
+      computers: [],
+      loading: true
     }
   },
   mounted () {
     this.$computersResource = this.$resource('computers')
     this.$computersResource.get().then(response => {
       this.computers = response.body
+      this.loading = false
     })
   }
 }
